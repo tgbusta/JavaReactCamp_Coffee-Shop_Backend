38dd93f4140c09ea610c202edd0996e70155e533
tgbusta <69412064+tgbusta@users.noreply.github.com>
2021-05-14 13:39:50 +0300

    JavaReactCamp_Lesson4

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+		customerManager.save(new Customer(1,"Tuðba","Usta",1980,"11278057528"));
+
+	}
+
+}
